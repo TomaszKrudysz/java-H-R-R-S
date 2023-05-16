d09dc401c10fa6f3dd94caf3c614673936dfa9a9
Tomek <t.krudysz@gmail.com>
2023-05-16 09:53:19 +0200

    geter seter

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        showSystemInfo();
+        String hotelName = "Overlook";
+        int systemVersion = 1;
+        boolean isDeveloperVersion = true;
+
+        showSystemInfo(hotelName,systemVersion,isDeveloperVersion);
 
         Scanner input = new Scanner(System.in);
 
@@ -12,19 +16,7 @@ public class App {
         if (option == 1) {
             Guest newGuest = createNewGuest(input);
         } else if (option == 2) {
-            try {
-                System.out.print("Numer: ");
-                int number = input.nextInt();
-                System.out.println("Ilość łóżek: ");
-                int beds = input.nextInt();
-                Room newRoom = new Room(number,beds);
-                String info = String.format("Dodano nowy pokój - numer: %d (%d)",
-                        newRoom.number,newRoom.beds);
-                System.out.println(info);
-            }catch (Exception e){
-                System.out.println("Podaj liczby");
-                e.printStackTrace();
-            }
+            Room newRoom = createNewRoom(input);
         } else if (option == 3) {
             System.out.println("Wybrano opcję 3");
         } else {
@@ -33,10 +25,8 @@ public class App {
 
     }
 
-    static void showSystemInfo(){
-        String hotelName = "Overlook";
-        int systemVersion = 1;
-        boolean isDeveloperVersion = true;
+    private static void showSystemInfo(String hotelName, int systemVersion, boolean isDeveloperVersion){
+
 
         System.out.println("Witamy w systemie rezerwacji dla hotelu " + hotelName);
         System.out.println("Aktualna wersja systemu: " + systemVersion);
@@ -46,7 +36,7 @@ public class App {
         System.out.println("\n================================\n");
     }
 
-    static int getActionFromUser(Scanner in){
+    private static int getActionFromUser(Scanner in){
         System.out.println("1. Dodaj nowego gościa");
         System.out.println("2. Dodaj nowy pokój");
         System.out.println("3. Wyszukaj gościa");
@@ -63,7 +53,7 @@ public class App {
 
         return actionNumber;
     }
-    static Guest createNewGuest (Scanner input){
+    private static Guest createNewGuest (Scanner input){
         try {
             System.out.println("Tworzymy nowego gościa");
             System.out.print("Podaj imię: ");
@@ -73,9 +63,7 @@ public class App {
             System.out.print("Podaj wiek: ");
             int age = input.nextInt();
             Guest newGuest = new Guest(firstName,lastName,age);
-            String info = String.format("Dodano nowego gościa: %s %s (%d)",
-                    newGuest.firstName, newGuest.lastName, newGuest.age);
-            System.out.println(info);
+            System.out.println(newGuest.getInfo());
             return newGuest;
         } catch (Exception e){
             System.out.println("Zły wiek, używaj liczb");
@@ -84,4 +72,22 @@ public class App {
         }
 
     }
+    private static Room createNewRoom(Scanner input){
+        try {
+            System.out.print("Numer: ");
+            int number = input.nextInt();
+            System.out.println("Ilość łóżek: ");
+            int beds = input.nextInt();
+            Room newRoom = new Room(number,beds);
+            String info = String.format("Dodano nowy pokój - numer: %d (%d)",
+                    newRoom.number,newRoom.beds);
+            System.out.println(info);
+            return newRoom;
+        }catch (Exception e){
+            System.out.println("Podaj liczby");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -1,8 +1,8 @@
 public class Guest {
 
-    String firstName;
-    String lastName;
-    int age;
+    private String firstName;
+    private String lastName;
+    private int age;
 
 
     public Guest(String firstName, String lastName, int age) {
@@ -10,5 +10,31 @@ public class Guest {
         this.lastName = lastName;
         this.age = age;
 
+    }
+    public String getInfo(){
+        return String.format("Dodano nowego gościa: %s %s (%d)",
+                this.firstName,this.lastName,this.age);
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public void setFirstName(String newFirstName){
+        this.firstName = newFirstName;
+    }
+
+    public java.lang.String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(java.lang.String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
